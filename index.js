5e9ab961966555c021cf79c7687e04ade0ac4d81
Ivan Borgo Battochio <ivan.borgo@viewsoftwarehouse.com>
2022-06-14 23:41:13 -0300

    general fixes

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config"
-import mongodb from "mongodb"
+import express, { json } from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import mongodb from 'mongodb'
 
 const app = express()
+
+app.use(json())
+
+app.use(
+	cors({
+		origin: '*',
+	})
+)
 
 var database = undefined
 
@@ -13,43 +21,58 @@ const dbConnect = async () => {
 	let client = new mongodb.MongoClient(uri)
 	await client.connect()
 
-	database = client.db("collection")
+	database = client.db('collection')
 }
 
-app.post("/insert-moves", async (req, res) => {
+app.post('/insert-moves', async (req, res) => {
 	const data = req.body
 	const winnerMoves = data.winnerMoves
 	const lossesMoves = data.lossesMoves
-	const collection = database.collection("moves")
+	const collection = database.collection('moves')
 
-	for (const move of winnerMoves) {
-		const query = { left: move.left, right: move.right }
-		const dbMove = await collection.findOne(query)
-		if (dbMove) {
-			dbMove.winsCounter++
-			await collection.updateOne(query, dbMove)
-		} else {
-			move.winsCounter = 1
-			move.lossesCounter = 0
-			await collection.insertOne(move)
+	try {
+		for (const move of winnerMoves) {
+			const query = { left: move.left, right: move.right }
+			const dbMove = await collection.findOne(query)
+			if (dbMove) {
+				await collection.updateOne(query, {
+					$set: { winsCounter: dbMove.winsCounter + 1 },
+				})
+			} else {
+				await collection.insertOne({
+					left: move.left,
+					right: move.right,
+					winsCounter: 1,
+					lossesCounter: 0,
+				})
+			}
 		}
-	}
 
-	for (const move of lossesMoves) {
-		const query = { left: move.left, right: move.right }
-		const dbMove = await collection.findOne(query)
-		if (dbMove) {
-			dbMove.lossesCounter++
-			await collection.updateOne(query, dbMove)
-		} else {
-			move.winsCounter = 0
-			move.lossesCounter = 1
-			await collection.insertOne(move)
+		for (const move of lossesMoves) {
+			const query = { left: move.left, right: move.right }
+			const dbMove = await collection.findOne(query)
+			if (dbMove) {
+				await collection.updateOne(query, {
+					$set: { lossesCounter: dbMove.lossesCounter + 1 },
+				})
+			} else {
+				await collection.insertOne({
+					left: move.left,
+					right: move.right,
+					winsCounter: 0,
+					lossesCounter: 1,
+				})
+			}
 		}
+
+		res.sendStatus(200)
+	} catch (e) {
+		console.log(e)
+		res.sendStatus(500)
 	}
 })
 
-app.get("/choose", async (req, res) => {
+app.post('/choose', async (req, res) => {
 	const data = req.body
 	const possibleMoves = data.possibilities
 
@@ -59,17 +82,22 @@ app.get("/choose", async (req, res) => {
 
 	const query = { $in: rightsAndLeftsArray }
 
-	const chosenMove = possibleMoves.reduce(() => {}, Promise.resolve())
+	const collection = database.collection('moves')
+
+	collection.find()
+	//const chosenMove = possibleMoves.reduce(() => {}, Promise.resolve())
 })
 
-const start = (async (port) => {
+const start = async (port) => {
 	try {
 		await dbConnect()
 
 		app.listen(port, () => {
-			console.log("listening on port " + port)
+			console.log('listening on port ' + port)
 		})
 	} catch (error) {
 		console.log(error)
 	}
-})(process.env.PORT)
+}
+
+start(process.env.PORT)
